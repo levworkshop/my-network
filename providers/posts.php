8c39fdf81ca0c87e205ccf757e555c0f7b3c2279
levworkshop <noreply@githubusers.com>
2023-01-19 20:10:58 +0200

    update example

--- providers/posts.php
+++ providers/posts.php
@@ -16,14 +16,32 @@ class Post
     {
         return $this->$property;
     }
+
+    protected function update($id, $title, $last_update, $author)
+    {
+        // update class properties
+    }
 }
 
 class TextPost extends Post
 {
     public $post_body;
+
+    public function update($body, $id, $title, $last_update, $author)
+    {
+        $this->post_body = $body;
+        parent::update($id, $title, $last_update, $author);
+    }
 }
 
 class ImagePost extends Post
 {
     public $image_url, $image_alt;
+
+    public function update($url, $alt, $id, $title, $last_update, $author)
+    {
+        $this->image_url = $url;
+        $this->image_alt = $alt;
+        parent::update($id, $title, $last_update, $author);
+    }
 }
