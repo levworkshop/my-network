0dc5f7c3ccdf2936139eb33c6dcbae6c780f533a
levworkshop <noreply@githubusers.com>
2023-01-23 20:45:44 +0200

    pdo

--- config/config.php
+++ config/config.php
@@ -2,5 +2,7 @@
 
 define('DB_URL', 'localhost');
 define('DB_USER', 'root');
-define('DB_PWD', 'password');
+define('DB_PWD', '');
 define('DB_NAME', 'my-network');
+
+define('PDO_CFG', 'mysql:host=localhost;dbname=my-network;charset=utf8');
